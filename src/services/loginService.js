b5f0f9d016fcafb4e9e47e275c14baff204cbe28
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-13 19:40:21 -0300

    feat: Cria a função para criação de token durante o login

---
+++ src/services/loginService.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { User } = require('../database/models');
+
+const secret = process.env.JWT_SECRET;
+
+const postLogin = async (email, password) => {
+  const user = await User.findOne({ where: { email } });
+
+  if (!user || user.password !== password) return 'invalid';
+
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({ data: user }, secret, jwtConfig);
+  return token;
+};
+
+module.exports = {
+  postLogin,
+};
