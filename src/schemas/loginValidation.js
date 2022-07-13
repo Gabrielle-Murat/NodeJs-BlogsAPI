7bbe6f746a5d5cf76a024cb562881f6e4dbe5d95
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-13 20:04:36 -0300

    fix: Conserta erro de validação

--- src/schemas/loginValidation.js
+++ src/schemas/loginValidation.js
@@ -4,14 +4,14 @@ module.exports = Joi.object({
   email: Joi.string()
     .required()
     .messages({
-      'any.required': '400|Some required fields are missing',
+      'string.empty': '400|Some required fields are missing',
       'string.base': '422|Invalid format',
     }),
 
   password: Joi.string()
     .required()
     .messages({
-      'any.required': '400|Some required fields are missing',
+      'string.empty': '400|Some required fields are missing',
       'string.base': '422|Invalid format',
     }),
 });
