7f3866838ff748de3b5efcf629588851bc9c55f5
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-13 19:43:06 -0300

    feat: Adiciona router de Login ao api.js

--- src/api.js
+++ src/api.js
@@ -1,13 +1,11 @@
 const express = require('express');
 
-// ...
+const loginRouter = require('./middlewares/loginRouter');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use('/login', loginRouter);
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
