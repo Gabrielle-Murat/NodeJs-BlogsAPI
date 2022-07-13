3fe1fd9902769aa60ef5f6e20f6664d285eab263
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-13 19:42:23 -0300

    feat: Cria rota de login

---
+++ src/middlewares/loginRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const Login = require('../controllers/loginController');
+const loginValidation = require('./loginValidation');
+
+const router = express.Router();
+
+router.post(
+  '/',
+  loginValidation,
+  Login.postLogin,
+);
+
+module.exports = router;
