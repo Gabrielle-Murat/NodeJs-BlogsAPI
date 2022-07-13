25a2f85b6abb84259ef0596ce6746b212df427e3
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-13 20:05:09 -0300

    chore: Remove comentários

--- src/middlewares/loginValidation.js
+++ src/middlewares/loginValidation.js
@@ -1,16 +1,11 @@
 const loginData = require('../schemas/loginValidation');
 
 function loginValidation(req, res, next) {
-  // console.log(req.body);
   const { error } = loginData.validate(req.body);
-  // console.log('oi');
-  // console.log(error);
-  // console.log(value);
 
   if (error) {
     const [code, message] = error.message.split('|');
     return res.status(parseInt(code, 10)).json({ message });
-    // return res.status(400).json({ message: 'Some required fields are missing' });
   }
 
   return next();
