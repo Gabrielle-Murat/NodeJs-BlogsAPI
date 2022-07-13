a6dfba49d6b9eefc99a4e3d6fa54b43819f7ca82
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-13 19:41:39 -0300

    feat: Cria schema de validação de login

---
+++ src/schemas/loginValidation.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+
+module.exports = Joi.object({
+  email: Joi.string()
+    .required()
+    .messages({
+      'any.required': '400|Some required fields are missing',
+      'string.base': '422|Invalid format',
+    }),
+
+  password: Joi.string()
+    .required()
+    .messages({
+      'any.required': '400|Some required fields are missing',
+      'string.base': '422|Invalid format',
+    }),
+});
