e9eca0cf9a9bc0e3bad7a1c258021a406827b2a3
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-13 19:40:57 -0300

    feat: Cria controller de Login

---
+++ src/controllers/loginController.js
@@ -0,0 +1,15 @@
+const Login = require('../services/loginService');
+
+const postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const loginResponse = await Login.postLogin(email, password);
+
+  if (loginResponse === 'invalid') {
+    return res.status(400).json({ message: 'Invalid fields' });
+  }
+  return res.status(200).json({ token: loginResponse });
+};
+
+module.exports = {
+  postLogin,
+};
