931e365cb9e2a57eb7a9d4fbdbec97ae6704f8d9
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-14 16:09:05 -0300

    feat: Cria função para buscar todas as Categories - Camada Controller

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,5 +1,7 @@
 const Category = require('../services/categoriesService');
 
+// requisito 8
+
 const createCategory = async (req, res) => {
   const { name } = req.body;
 
@@ -12,6 +14,17 @@ const createCategory = async (req, res) => {
   return res.status(201).json(response);
 };
 
+// requisito 9
+
+const getCategories = async (_req, res) => {
+  const categories = await Category.getCategories();
+
+  if (!categories) return res.status(500).json({ message: 'Something went wrong' });
+
+  return res.status(200).json(categories);
+};
+
 module.exports = {
   createCategory,
+  getCategories,
 };
