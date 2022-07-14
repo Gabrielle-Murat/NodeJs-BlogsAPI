d0940a2564bec8b317113a5df5af2550118c3d77
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-14 13:23:32 -0300

    feat: Cria validação de Users

---
+++ src/middlewares/userValidation.js
@@ -0,0 +1,14 @@
+const userData = require('../schemas/userValidation');
+
+function userValidation(req, res, next) {
+  const { error } = userData.validate(req.body);
+
+  if (error) {
+    const [code, message] = error.message.split('|');
+    return res.status(parseInt(code, 10)).json({ message });
+  }
+
+  return next();
+}
+
+module.exports = userValidation;
