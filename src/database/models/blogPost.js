a8cd280e5913de94074c80df0caeb0012f94c14d
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-14 16:25:37 -0300

    feat: Cria model de BlogPost

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,26 @@
+// requisito 10
+
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    timestamps: true,
+    createdAt: 'published',
+    updatedAt: 'updated',
+    tableName: 'BlogPosts',
+  });
+
+  return BlogPost;
+};
