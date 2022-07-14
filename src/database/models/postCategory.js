2a99d73871696b13a1c0674315fbed2bb1f5a2e0
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-14 16:48:29 -0300

    feat: Cria associações entre as tabelas PostCategories, BlogPosts e Categories
    Associações construídas no model de PostCategory

--- src/database/models/postCategory.js
+++ src/database/models/postCategory.js
@@ -18,5 +18,21 @@ module.exports = (sequelize, DataTypes) => {
   },
   { timestamps: false });
 
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+  }
+
   return PostCategory;
 };
