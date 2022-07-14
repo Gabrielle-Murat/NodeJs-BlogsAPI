2e8a93ebd95c578916ca441e70c9b3a1a60e8291
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-14 16:10:12 -0300

    feat: Cria rota para buscar todas as Categories
    Endpoint GET com validação prévia de token

--- src/middlewares/routers/categoriesRouter.js
+++ src/middlewares/routers/categoriesRouter.js
@@ -9,4 +9,7 @@ const router = express.Router();
 // requisito 8
 router.post('/', categoryValidation, validateJWT, Category.createCategory);
 
+// requisito 9
+router.get('/', validateJWT, Category.getCategories);
+
 module.exports = router;
