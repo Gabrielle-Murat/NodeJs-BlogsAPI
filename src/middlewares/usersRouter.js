a129f11bc15f4b7e75c113840b137aeb262bbdc7
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-14 14:49:33 -0300

    feat: Cria rota para buscar todos os users

--- src/middlewares/usersRouter.js
+++ src/middlewares/usersRouter.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const User = require('../controllers/usersController');
 const userValidation = require('./userValidation');
+const { validateJWT } = require('./auth/tokenValidation');
 
 const router = express.Router();
 
 // requisito 4
 router.post('/', userValidation, User.createUser);
+
+// requisito 5
+router.get('/', validateJWT, User.getUsers);
 
 module.exports = router;
