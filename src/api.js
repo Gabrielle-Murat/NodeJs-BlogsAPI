745f98f1417d1a05fded8f4ae6166c7dec1225bb
Gabrielle-Murat <belle.murat@gmail.com>
2022-07-14 13:24:35 -0300

    feat: Adiciona router de Users ao api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const loginRouter = require('./middlewares/loginRouter');
+const usersRouter = require('./middlewares/usersRouter');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/login', loginRouter);
+
+app.use('/user', usersRouter);
 
 module.exports = app;
